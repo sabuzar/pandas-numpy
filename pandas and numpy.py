9dd30545dd5e9d33127f9570e5ae03109e38b9c6
Abu Zar <103764167+sabuzar@users.noreply.github.com>
2022-10-03 09:06:27 -0700

    pandas and numpy
    basic code

---
+++ pandas and numpy.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+dict1 = {
+    "name":['abuzar', 'yousha', 'muaz', 'zain'],
+    "marks":[5,10,7,15],
+    "city":['chiniot','faislabad','dubai','salara']
+}
+fw = pd.DataFrame(dict1)
+print(fw)
